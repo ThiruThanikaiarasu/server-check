6a46a399ed61ebada976bbc50be374ae9daa744b
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-02-19 22:58:25 +0530

    FIX. Fixed the Port number issue.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000 
 
 app.get('/', (request, response) => {
     response.status(200).json({message: "It's working"})
 })
 
-app.listen(process.env.PORT)
+app.listen(PORT, console.log(`Server is running at http://localhost:3500`))
 
 
